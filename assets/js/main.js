c9de133dfbb6c76b9544bf131b9d942f5b732371
RAMY MAJED  ALI <ramy.majed@kualitatem.com>
2026-09-05 16:50:16 +0500

    Update portfolio with current resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-  strings: ["Frontend Developer" , "Web Developer" , "Backend Developer"],
+    strings: ["Software QA Engineer" , "Manual Tester" , "Frontend Developer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
